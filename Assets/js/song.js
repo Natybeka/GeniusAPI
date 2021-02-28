76aabb811c91080660ab32656268da7ce02c3fc2
Eden Mesfin <se.eden.mesfin@gmail.com>
2021-02-28 04:36:14 +0300

    fetch the selected song from api

--- Assets/js/song.js
+++ Assets/js/song.js
@@ -1,3 +1,18 @@
 const songView = document.querySelector("#songLyrics");
 const albumArt = document.querySelector('#image-container');
 const songLyrics = document.querySelector("#lyrics-container");
+
+document.addEventListener('DOMContentLoaded', getSong);
+function getSong() { 
+    let songID = sessionStorage.getItem('songID');
+    // console.log(songID);
+        // fetch data
+        fetch("https://genius.p.rapidapi.com/songs/"+songID, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "53485a8fadmsh7bfa6acdf627f77p166324jsncf4e61281f8d",
+                "x-rapidapi-host": "genius.p.rapidapi.com"
+            }
+        }).then(res => res.json())
+
+}
