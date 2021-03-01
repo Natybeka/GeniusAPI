ace2d959551a703ee9360e7f8934e536c05be5b8
Yafet Lulseged <se.yafet.lulseged@gmail.com>
2021-03-01 14:03:29 +0300

    Fifth Commit - Iterate and fetch song using song id

--- Assets/js/putFav.js
+++ Assets/js/putFav.js
@@ -12,5 +12,19 @@ function putFavourites() {
     if(favSong.length === 0){
         return;
     }
-      
+    favSong.forEach(songs => {
+        // fetch data
+        fetch("https://genius.p.rapidapi.com/songs/"+songs, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "53485a8fadmsh7bfa6acdf627f77p166324jsncf4e61281f8d",
+                "x-rapidapi-host": "genius.p.rapidapi.com"
+            }
+        }).then(res => res.json())
+        .then(jsonFormat => jsonFormat.response)
+        .then(response => response.song)
+        .catch((error) => {
+            console.log(error)
+        });
+    });   
 }
